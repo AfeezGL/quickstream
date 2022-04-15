319d33f99dcb6a9550c68e858bec7cab9cda379d
Afeez G Lawal <afeezlg@gmail.com>
2022-04-15 11:32:23 +0100

    completed basic app flow

--- src/components/CreateStream.js
+++ src/components/CreateStream.js
@@ -3,28 +3,30 @@ import React, { useEffect, useRef, useState } from "react";
 import Auth from "./Auth";
 import LiveChat from "./LiveChat";
 
+const rtc = {
+  // For the local audio and video tracks.
+  localAudioTrack: null,
+  localVideoTrack: null,
+  client: null,
+};
+
+const options = {
+  appId: "7ac8167595aa47aeb4ddf6b34353ec38",
+  channel: "test",
+  token:
+    "0067ac8167595aa47aeb4ddf6b34353ec38IAA+7QV4e0KyD+TfKeNir/s+w6kjMK/QB1sBCSubDJQUlQx+f9gAAAAAEAA7zd8LXIZaYgEAAQBchlpi",
+  uid: 12,
+};
+
+rtc.client = AgoraRTC.createClient({ mode: "live", codec: "vp8" });
+
 const CreateStream = ({ user }) => {
   const [streamId, setStreamId] = useState("");
   const myStream = useRef();
 
-  const rtc = {
-    // For the local audio and video tracks.
-    localAudioTrack: null,
-    localVideoTrack: null,
-    client: null,
-  };
-
-  const options = {
-    appId: "7ac8167595aa47aeb4ddf6b34353ec38",
-    channel: "test",
-    token: "19edc60b759d4bd2a699c7faa23f91cf",
-    uid: 123456,
-  };
-
-  setStreamId(options.uid);
-
   useEffect(() => {
     const startSream = async () => {
+      setStreamId(options.channel);
       // create client
       rtc.client = AgoraRTC.createClient({ mode: "live", codec: "vp8" });
       rtc.client.setClientRole("host");
@@ -43,7 +45,7 @@ const CreateStream = ({ user }) => {
       // Publish the local audio and video tracks to the channel.
       await rtc.client.publish([rtc.localAudioTrack, rtc.localVideoTrack]);
 
-      rtc.localVideoTrack.play();
+      rtc.localVideoTrack.play(myStream.current);
 
       console.log("publish success!");
     };
@@ -57,7 +59,7 @@ const CreateStream = ({ user }) => {
       <Auth user={user} />
       <div className="streamContainer">
         <div className="video">
-          <video ref={myStream} muted autoPlay />
+          <div ref={myStream} className="video-container"></div>
           <p>{streamId}</p>
         </div>
         {streamId && <LiveChat streamId={streamId} />}
--- src/components/WatchSream.js
+++ src/components/WatchSream.js
@@ -1,58 +1,89 @@
-import Peer from "peerjs";
-import React, { useEffect, useRef } from "react";
+import AgoraRTC from "agora-rtc-sdk-ng";
+import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router";
-import { uuid } from "uuidv4";
 import Auth from "./Auth";
 import LiveChat from "./LiveChat";
 
+const rtc = {
+  // For the local audio and video tracks.
+  localAudioTrack: null,
+  localVideoTrack: null,
+  client: null,
+};
+
+const options = {
+  appId: "7ac8167595aa47aeb4ddf6b34353ec38",
+  channel: "test",
+  token:
+    "0067ac8167595aa47aeb4ddf6b34353ec38IAA+7QV4e0KyD+TfKeNir/s+w6kjMK/QB1sBCSubDJQUlQx+f9gAAAAAEAA7zd8LXIZaYgEAAQBchlpi",
+  uid: 15,
+};
+
+rtc.client = AgoraRTC.createClient({ mode: "live", codec: "vp8" });
+
 const WatchSream = ({ user }) => {
-    const { streamId } = useParams();
-    const videoStream = useRef();
+  const { streamId } = useParams();
+  const [showStream, setShowStream] = useState(false);
+  const videoStream = useRef();
 
-    useEffect(() => {
-        const uid = uuid();
-        const peer = new Peer(uid, {
-            host: "/",
-            port: process.env.NODE_ENV === "development" ? 9000 : 443,
-            path: "/peerjs",
-            secure: process.env.NODE_ENV === "development" ? false : true,
-        });
+  useEffect(() => {
+    const getStream = async () => {
+      rtc.client.setClientRole("audience");
+      await rtc.client.join(
+        options.appId,
+        options.channel,
+        options.token,
+        options.uid
+      );
 
-        peer.on("open", () => {
-            console.log("open");
-            const connection = peer.connect(streamId);
-            connection.on("open", () => {
-                console.log("connection successful");
-                connection.send("hello");
-                connection.on("data", (data) => {
-                    console.log(data);
-                });
-            });
-            connection.on("error", (error) => {
-                console.log(error);
-            });
-        });
+      rtc.client.on("user-published", async (user, mediaType) => {
+        // Subscribe to a remote user.
+        await rtc.client.subscribe(user, mediaType);
+        console.log("subscribe success");
 
-        peer.on("call", (call) => {
-            console.log("incoming call");
-            call.answer();
-            call.on("stream", (stream) => {
-                videoStream.current.srcObject = stream;
-                console.log("recieving stream");
-            });
-        });
-    }, [streamId]);
+        if (mediaType === "video") {
+          // show video stream
+          setShowStream(true);
 
-    return (
-        <div>
-            <h1>Watch stream</h1>
-            <Auth user={user} />
-            <div className="streamContainer">
-                <video className="video" ref={videoStream} muted autoPlay />
-                <LiveChat streamId={streamId} />
-            </div>
-        </div>
-    );
+          // Get `RemoteVideoTrack` in the `user` object.
+          const remoteVideoTrack = user.videoTrack;
+
+          // Play the remote video track.
+          remoteVideoTrack.play(videoStream.current);
+        }
+
+        // If the subscribed track is audio.
+        if (mediaType === "audio") {
+          // Get `RemoteAudioTrack` in the `user` object.
+          const remoteAudioTrack = user.audioTrack;
+          // Play the audio track. No need to pass any DOM element.
+          remoteAudioTrack.play();
+        }
+      });
+    };
+
+    getStream();
+
+    return async () => {
+      // Leave the channel.
+      await rtc.client.leave();
+    };
+  }, [streamId]);
+
+  return (
+    <div>
+      <h1>Watch stream</h1>
+      <Auth user={user} />
+      <div className="streamContainer">
+        {!showStream ? (
+          <div className="no-video">No Video Stream</div>
+        ) : (
+          <div className="video-container" ref={videoStream}></div>
+        )}
+        <LiveChat streamId={streamId} />
+      </div>
+    </div>
+  );
 };
 
 export default WatchSream;
