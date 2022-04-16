5dbfd0e848aaab4c51b27b31307cb7bcbfccfeee
Afeez G Lawal <afeezlg@gmail.com>
2022-04-16 07:12:16 +0100

    revamping the homepage - added header and call to action

--- src/components/HomeScreem.js
+++ src/components/HomeScreem.js
@@ -1,25 +1,49 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 const HomeScreem = () => {
-    const [streamId, setstreamId] = useState("");
+  const [streamId, setstreamId] = useState("");
+  const history = useHistory();
 
-    return (
-        <div>
-            <h1>HomeScreen</h1>
-            <Link to="/stream">Create a stream</Link>
-            <h2>Watch a stream</h2>
-            <input
-                type="text"
-                name="streamId"
-                placeholder="paste stream id"
-                onChange={(e) => {
-                    setstreamId(e.target.value);
-                }}
-            />
-            <Link to={`watch/${streamId}`}>Watch Stream</Link>
-        </div>
-    );
+  //   navigate to watchstream page when user submits form
+  const submit = (e) => {
+    e.preventDefault();
+    history.push(`watch/${streamId}`);
+  };
+
+  return (
+    <>
+      <header>
+        <h1>QuickStream</h1>
+        <p>
+          Connect with your friends and fans without the hassels of special
+          softwares and signups signups
+        </p>
+        <p>The stress free way to watch your favourite people do their thing</p>
+      </header>
+      <div className="call-to-action">
+        <Link to="/stream" className="create-stream-btn">
+          Create a stream
+        </Link>
+        <form onSubmit={submit}>
+          <h2>Watch a stream</h2>
+          <input
+            type="text"
+            name="streamId"
+            placeholder="paste stream id"
+            onChange={(e) => {
+              setstreamId(e.target.value);
+            }}
+          />
+          <div className="submit-btn-container">
+            <input type="submit" value="Watch Stream" />
+          </div>
+
+          {/* <Link to={`watch/${streamId}`}>Watch Stream</Link> */}
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default HomeScreem;
