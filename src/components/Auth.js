3d66453f0820e44ab0bf4b92c32f28fc43ef6a6a
Afeez G Lawal <afeezlg@gmail.com>
2022-04-19 17:50:21 +0100

    added icons to authentication buttons

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -2,23 +2,27 @@ import React from "react";
 import { auth, provider } from "../firebase";
 
 const Auth = ({ user }) => {
-    const login = () => {
-        auth.signInWithPopup(provider);
-    };
+	const login = () => {
+		auth.signInWithPopup(provider);
+	};
 
-    const logout = () => {
-        auth.signOut();
-    };
+	const logout = () => {
+		auth.signOut();
+	};
 
-    return (
-        <div>
-            {!user ? (
-                <button onClick={login}>login</button>
-            ) : (
-                <button onClick={logout}>logout</button>
-            )}
-        </div>
-    );
+	return (
+		<div className="auth">
+			{!user ? (
+				<button onClick={login}>
+					<i className="fa-solid fa-arrow-right-to-bracket"></i> Login
+				</button>
+			) : (
+				<button onClick={logout}>
+					<i className="fa-solid fa-arrow-right-from-bracket"></i> Logout
+				</button>
+			)}
+		</div>
+	);
 };
 
 export default Auth;
